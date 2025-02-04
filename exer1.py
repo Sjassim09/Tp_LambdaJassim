0cae484c2436a6aea8108d3c768a89b436c64981
Sjassim09 <jassimsirat@gmail.com>
2025-02-04 10:26:58 +0100

    EXER6 et 7

--- exer1.py
+++ exer1.py
@@ -44,7 +44,6 @@ for word in words:
 print("Nombre de mots > 5 caractères:", word_counter("") )
 
 #Exercice 5:
-
 def compose(f, g):
     return lambda x: f(g(x))
 
@@ -53,3 +52,34 @@ f2 = lambda x: x * 3
 composed_function = compose(f1, f2)
 print("Composée:", composed_function(4))
 
+#Exercice 6:
+def filterMap(filter_func, map_func, lst):
+    return [map_func(x) for x in lst if filter_func(x)]
+
+words = ["", "hello", "world", "python", ""]
+result = filterMap(lambda x: x != "", lambda x:
+x.upper(), words)
+print(result) # ['HELLO', 'WORLD', 'PYTHON']
+
+#Exercice 7:
+def memoize(f):
+    cache = {}
+    def memoized_function(x):
+        if x not in cache:
+            cache[x] = f(x)
+        return cache[x]
+    return memoized_function
+
+@memoize
+def factorial(n):
+    return 1 if n == 0 else n * factorial(n-1)
+
+@memoize
+def fibonacci(n):
+    return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+
+print("Factorielle de 5:", factorial(5))
+print("Fibonacci de 10:", fibonacci(10))
+
+
+#Exercice 8:
