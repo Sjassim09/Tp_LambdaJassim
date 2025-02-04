f154a4230981eb33cd22f018914ce2b24b7e9530
Sjassim09 <jassimsirat@gmail.com>
2025-02-04 10:07:04 +0100

    1.4

--- exer1.py
+++ exer1.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 square = lambda x: x ** 2
 print(square(5))
 
@@ -8,3 +10,5 @@ print("Carrés:", squared_numbers)
 sum_two = lambda a, b: a + b
 print(sum_two(3, 4))
 
+total_sum = reduce(sum_two, numbers)
+print("Somme totale:", total_sum)
